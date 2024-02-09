e01ed70c7dc28b532544bc340a99c3df5003a5ff
Adysson <afsilva9@stefanini.com>
2024-02-09 13:14:45 -0300

    fix: ajuste do model Statement

--- model/extrato.go
+++ model/extrato.go
@@ -3,4 +3,5 @@ package model
 type Statement struct {
 	Balance     Balance      `json:"saldo"`
 	Tansactions []Tansaction `json:"ultimas_transacoes"`
+	Date        string       `json:"data_extrato"`
 }
